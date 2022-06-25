82b38d0ebe6fe9a5e7a2198c401eed0bddc1b407
taotang <tangtao_mail@sina.com>
2022-06-25 16:39:00 +0800

    feat: 添加Ts定义

---
+++ src/screens/project-list/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { SearchPanel } from "./search-panel";
+import { List } from "./list";
+import { useEffect, useState } from "react";
+import * as qs from "qs";
+import { cleanObject, useDebounce, useMount } from "../../utils";
+const apiUrl = process.env.REACT_APP_API_URL;
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+
+  const debouncedParam = useDebounce(param, 200);
+
+  const [users, setUsers] = useState([]);
+
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
+      async (response) => {
+        if (response.ok) {
+          setList(await response.json());
+        }
+      }
+    );
+  }, [debouncedParam]);
+
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  });
+
+  return (
+    <div>
+      <SearchPanel param={param} setParam={setParam} users={users} />
+      <List users={users} list={list} />
+    </div>
+  );
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,16 +1,18 @@
 import { useEffect, useState } from "react";
-export const isFalsy = (value) => (value === 0 ? false : !value);
-export const cleanObject = (object) => {
+export const isFalsy = (value: any) => (value === 0 ? false : !value);
+export const cleanObject = (object: object) => {
   const result = { ...object };
   Object.keys(result).forEach((key) => {
+    // @ts-ignore
     const value = result[key];
     if (isFalsy(value)) {
+      // @ts-ignore
       delete result[key];
     }
   });
   return result;
 };
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
   }, []);
@@ -35,7 +37,7 @@ export const useMount = (callback) => {
 // log({name:"1"});
 // log({name:"1"});
 
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value: any, delay?: number) => {
   const [debounceValue, setDebounceValue] = useState(value);
 
   useEffect(() => {
