0f22b57d82732f20bc6ece735f9bde880234e6c7
taotang <tangtao_mail@sina.com>
2022-06-25 15:40:00 +0800

    feat: 防抖函数

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -3,7 +3,7 @@ import {SearchPanel} from './search-panel';
 import {List} from './list';
 import {useEffect, useState} from "react";
 import qs from "qs";
-import {cleanObject} from "../../utils";
+import {cleanObject, useDebounce, useMount} from "../../utils";
 const apiUrl = process.env.REACT_APP_API_URL;
 export const ProjectListScreen = () =>{
 
@@ -11,6 +11,8 @@ export const ProjectListScreen = () =>{
         name:"",
         personId:""
     });
+
+    const debouncedParam = useDebounce(param,2000);
 
     const [users,setUsers] = useState([])
 
@@ -22,15 +24,15 @@ export const ProjectListScreen = () =>{
                 setList(await response.json());
             }
         })
-    },[param])
+    },[debouncedParam])
 
-    useEffect(()=>{
+    useMount(()=>{
         fetch(`${apiUrl}/users`).then(async response=>{
             if(response.ok){
                 setUsers(await response.json());
             }
         })
-    },[param])
+    })
 
     return <div>
         <SearchPanel param={param} setParam={setParam} users={users}/>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 export const isFalsy = (value) => (value === 0 ? false : !value);
 export const cleanObject = (object) => {
   const result = { ...object };
@@ -8,4 +9,41 @@ export const cleanObject = (object) => {
     }
   });
   return result;
+};
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+// 延迟函数编写
+// const debounce = (func,delay) =>{
+//   let timeout;
+//   return (...param)=>{
+//     if(timeout){
+//       clearTimeout(timeout);
+//     }
+//     timeout = setTimeout(function () {
+//       func(...param);
+//     },delay)
+//   }
+// }
+//
+// const log = debounce((param)=> console.log('call',param),5000);
+//
+// log({name:"1"});
+// log({name:"1"});
+// log({name:"1"});
+
+export const useDebounce = (value, delay) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+
+  useEffect(() => {
+    // 每次在Value变化之后，设置一个定时器
+    const timeout = setTimeout(() => setDebounceValue(value), delay);
+    // 1.每次在上一个useEffect处理完成之后运行
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  return debounceValue;
 };
